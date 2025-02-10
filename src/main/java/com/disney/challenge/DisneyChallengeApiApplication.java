2cb9db53b76c226c8a577bab10f11455df3627d0
lucascheistwer <lucas.cheistwer@zerf.tech>
2025-02-10 11:55:46 -0300

    Initial Commit

---
+++ src/main/java/com/disney/challenge/DisneyChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.disney.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyChallengeApiApplication.class, args);
+	}
+
+}
